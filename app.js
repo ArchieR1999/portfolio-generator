5a55499c404edae1375de4d03e74564b42e0d859
ArchieR1999 <arseniy-v@hotmail.com>
2024-10-26 12:42:08 -0400

    added generate webpage functionality by inputing name and github name

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
-const printProfileData = (profileDataArr) => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+// this will allow to use the generatePage function from the src folder, not in this file. 
+const generatePage = require('./src/page-template.js');
 
-    console.log('=================');
+const profileDataArgs = process.argv.slice(2);
 
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
+const [name, github] = profileDataArgs;
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
     
-};
-
-printProfileData(profileDataArgs);
+    if (err) throw new Error (err);
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
