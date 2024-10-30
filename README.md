65908e6bbaec3e3fed9c4b0e4286043e8800a824
ArchieR1999 <arseniy-v@hotmail.com>
2024-10-30 17:31:55 -0400

    finilized changes, it works! And added README.md with detailed instructions

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# portfolio-generator
+# <portfolio-generator>
+
+## Description
+
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+- My goal was to create an application that took in user input using the node.js framework on the user computer and to dynamically generate the inputs into html files, updating all within the node.js framework itself. 
+- This 'app' was build using javascript, the ES6 module, some css to style the page, and node.js.
+- It helps a user to generate a portfolio of projects based on the user's answers and their GitHub repos without writing a seperate html file, eliminating the need for such tedious work.
+- I learned a lot about node.js and how I can use promises to circumvent the need for 'callback hell'. It was a really enjoyable experience.
+
+## Table of Contents (Optional)
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+This project requires node.js to run, so please install the latest version by going to their website and downloading it. Moreover, you must download/clone the repo in order to use it. It does not exist on a server somewhere in California... 
+
+## Usage
+
+Here is how to use the 'app'.
+1. You must launch the node file using your command line. It could be launched in the Git bash shell too. So in the terminal, write node app.js to get started.
+2. You will be asked questions that relate to how the final page will look. First, you will be asked your name, github name, and whether or not you want to include any information about yourself. If you choose 'no', you will not see anything in this section after you generate the html file. More on that later.
+3. When you finish the 'about' section, you will be asked about your **projects**. This is the 'crux' of this app, which will let you showcase your projects! So be descriptive! And another important thing: when asked if you'd like to feature this project, it will affect the final look of that section in the projects part. If you do not feature it, it will be stylized differently. If you feature it, the content will span the entire page and will be 'more visible' on the page. 
+4. After answering all the questions, you will be able to see the result by going into the 'dist' folder and launching the created html file. And voila... enjoy the generator!! 
+
+## Credits
+&copy Made with <3 by Archie 
+
+## License
+N/A, but here is a 🏆 instead :)
+---
--- app.js
+++ app.js
@@ -1,12 +1,6 @@
-// import inquirer from 'inquirer';
-const inquirer = require("inquirer");
-const { writeFile, copyFile } = require('./utils/generate-site.js');
+const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
-
-// const portfolioData = {
-//   user: {},
-//   projects: [] // initialize the projects array here.
-// };
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -26,9 +20,9 @@ const promptUser = () => {
     {
       type: 'input',
       name: 'github',
-      message: 'Enter your GitHub username. (Required)',
-      validate: gitInput => {
-        if (gitInput) {
+      message: 'Enter your GitHub Username (Required)',
+      validate: githubInput => {
+        if (githubInput) {
           return true;
         } else {
           console.log('Please enter your GitHub username!');
@@ -39,103 +33,99 @@ const promptUser = () => {
     {
       type: 'confirm',
       name: 'confirmAbout',
-      message: 'Would you like to enter some information about yourself in an "About" section?',
+      message: 'Would you like to enter some information about yourself for an "About" section?',
       default: true
     },
     {
-        type: 'input',
-        name: 'about',
-        message: 'Provide some information about yourself:',
-        when: ({ confirmAbout }) => confirmAbout
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:',
+      when: ({ confirmAbout }) => confirmAbout
     }
   ]);
 };
 
-// update promptProject to accept portfolioData as a parameter
 const promptProject = portfolioData => {
-// if there's no 'projects' array property, create one
-if (!portfolioData.projects) {
-  portfolioData.projects = []; // initialize it as an empty array on the first call
-}
   console.log(`
-
 =================
-
 Add a New Project
-
 =================
 `);
 
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is the name of your project? (Required)',
-      validate: projectInput => {
-        if (projectInput) {
-          return true;
-        } else {
-          console.log('Please enter your project name!');
-          return false;
+  // If there's no 'projects' array property, create one
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
+  return inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your project? (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('You need to enter a project name!');
+            return false;
+          }
         }
-      } 
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Please provide a description of the project. (Required)',
-      validate: projectDesc => {
-        if (projectDesc) {
-          return true;
-        } else {
-          console.log('Please enter a project description!');
-          return false;
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)',
+        validate: descriptionInput => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log('You need to enter a project description!');
+            return false;
+          }
         }
+      },
+      {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did you this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+      },
+      {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: linkInput => {
+          if (linkInput) {
+            return true;
+          } else {
+            console.log('You need to enter a project GitHub link!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'confirm',
+        name: 'feature',
+        message: 'Would you like to feature this project?',
+        default: false
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAddProject',
+        message: 'Would you like to enter another project?',
+        default: false
       }
-    },
-    {
-      type: 'checkbox',
-      name: 'languages',
-      message: 'What did you build this project with? (Check all that apply)',
-      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
-    },
-    {
-      type: 'input',
-      name: 'link',
-      message: 'Enter the GitHub link to your project. (Required)',
-      validate: projectLink => {
-        if (projectLink) {
-          return true;
-        } else {
-          console.log('Please enter the link to your project!');
-          return false;
-        }
+    ])
+    .then(projectData => {
+      portfolioData.projects.push(projectData);
+      if (projectData.confirmAddProject) {
+        return promptProject(portfolioData);
+      } else {
+        return portfolioData;
       }
-    },
-    {
-      type: 'confirm',
-      name: 'feature',
-      message: 'Would you like to feature this project?',
-      default: false
-    },
-    {
-      type: 'confirm',
-      name: 'confirmAddProject',
-      message: 'Would you like to enter another project?',
-      default: false
-    }
-  ])
-  .then(projectData => {
-    portfolioData.projects.push(projectData);
-    if (projectData.confirmAddProject) {
-      return promptProject(portfolioData);
-    } else {
-      return portfolioData;
-    }
-  });
+    });
 };
 
-// Start the prompt chain
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
@@ -154,69 +144,3 @@ promptUser()
   .catch(err => {
     console.log(err);
   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// old code below!!!
-
-// promptUser()
-//   .then(promptProject)
-//   .then(portfolioData => {
-//     // will be uncommented in lesson 4
-//     const pageHTML = generatePage(portfolioData);
-//     fs.writeFile('./dist/index.html', pageHTML, err => {
-//       if (err) {
-//         console.log(err);
-//         return;
-//       };
-      
-//       console.log('Page created! Check out index.html in this directory to see it!');
-      
-//       fs.copyFile('./src/style.css', './dist/style.css', err => {
-//         if (err) {
-//           console.log(err);
-//           return;
-//         }
-//         console.log('Stylesheet copied successfully!');
-//       });
-//   });
-// });
-
-// OLD CODE BELOW
-
-// const fs = require('fs');
-
-// // this will allow to use the generatePage function from the src folder, not in this file. 
-// const generatePage = require('./src/page-template.js');
-
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-    
-//     if (err) throw err;
-  
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
-
-
